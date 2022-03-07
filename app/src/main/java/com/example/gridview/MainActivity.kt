a378f305e2782dd1c6e639168bccb3f7e42ef65f
Karen Tamer <karen.poot@cbtis72.edu.mx>
2022-03-06 19:33:04 -0500

    Adaptador Personalizado

--- app/src/main/java/com/example/gridview/MainActivity.kt
+++ app/src/main/java/com/example/gridview/MainActivity.kt
@@ -12,24 +12,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var frutas = ArrayList<Fruta>()
 
-        var frutas = ArrayList<String>()
+        //frutas.add("Manzana")
+        //frutas.add("Plátano")
+        //frutas.add("Sandía")
+        //frutas.add("Durazno")
 
-        frutas.add("Manzana")
-        frutas.add("Plátano")
-        frutas.add("Sandía")
-        frutas.add("Durazno")
+        frutas.add(Fruta("Manzana", R.drawable.manzana))
+        frutas.add(Fruta("Plátano", R.drawable.platano))
+        frutas.add(Fruta("Sandía", R.drawable.sandia))
+        frutas.add(Fruta("Durazno", R.drawable.durazno))
+        frutas.add(Fruta("Manzana", R.drawable.manzana))
+        frutas.add(Fruta("Plátano", R.drawable.platano))
+        frutas.add(Fruta("Sandía", R.drawable.sandia))
+        frutas.add(Fruta("Durazno", R.drawable.durazno))
+        frutas.add(Fruta("Manzana", R.drawable.manzana))
+        frutas.add(Fruta("Plátano", R.drawable.platano))
+        frutas.add(Fruta("Sandía", R.drawable.sandia))
+        frutas.add(Fruta("Durazno", R.drawable.durazno))
 
-        var grid: GridView = findViewById(R.id.grid)
+        var grid:GridView = findViewById(R.id.grid)
 
-        val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,frutas)
+        //val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,frutas)
+
+        val adaptador = AdaptadorCustom(this, frutas)
 
         grid.adapter = adaptador
 
         grid.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
 
-            Toast.makeText(this, frutas.get(position), Toast.LENGTH_SHORT).show()}
+            Toast.makeText(this, frutas.get(position).nombre, Toast.LENGTH_SHORT).show()
 
+        }
     }
 
 }
